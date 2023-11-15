209804d2f51fcaa186131aa8bc79ac74435d7507
mikenitj <mikenitipong@hotmail.com>
2023-11-15 15:22:43 +0700

    fix login problem

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,11 +7,11 @@ const Login = () => {
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
-    // e.preventDefault();
+    e.preventDefault();
     try {
       // Make an API request to your login endpoint
       // Send username and password in the request
-      const response = await fetch("https://localhost:7000/user/login/", {
+      const response = await fetch("http://localhost:7000/user/login/", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -22,7 +22,7 @@ const Login = () => {
       if (response.ok) {
         // Login successful, redirect to the dashboard or another page
         const token = await response.json()
-        localStorage.setItem('accessToken', token.auth_token);
+        localStorage.setItem('accessToken', token.accessToken);
         console.log(token);
         navigate('/');
       } else {
@@ -58,3 +58,59 @@ const Login = () => {
 };
 
 export default Login;
+
+
+// import React, { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+
+// const Login = () => {
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+//   const navigate = useNavigate();
+
+//   const handleLogin = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const response = await fetch("https://localhost:7000/user/login/", {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ username, password }),
+//       });
+
+//       if (response.ok) {
+//         const token = await response.json()
+//         localStorage.setItem('accessToken', token.auth_token);
+//         console.log(token);
+//         navigate('/');
+//       } else {
+//       }
+//     } catch (error) {
+//       console.error('Login error:', error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h2>Login</h2>
+//       <form onSubmit={handleLogin}>
+//         <input
+//           type="text"
+//           placeholder="Username"
+//           value={username}
+//           onChange={(e) => setUsername(e.target.value)}
+//         />
+//         <input
+//           type="password"
+//           placeholder="Password"
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//         />
+//         <button type="submit">Login</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Login;
